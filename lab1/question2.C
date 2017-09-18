c63ba597d62264257aa278ed53c40c2b7107b5a4
Zarah Gutierrez <zarahgutierrez@gmail.com>
2017-09-18 13:41:52 -0700

    Finished ish question 2

--- lab1/question2.C
+++ lab1/question2.C
@@ -5,21 +5,26 @@ int main()
    unsigned char bytestring[] = {0x41, 0x33, 0x54, 0x80, 0xFF, 0x99, 0x01, 0x78,0x55, 0x20, 0xFE, 0xEE, 0x00, 0x00, 0x00, 0x00 };
    int length = sizeof(bytestring)/sizeof(bytestring[0]);
    
-   short int *s;
-   int *i;
-   long int *l;  
- 
+   printf("Unsigned version: ");
    for (int n = 0; n < length; n++)
    {
-      printf("This is the unsigned version: %u, and it has a size of %lu\n", bytestring[n], sizeof(bytestring[n]));
-      short int var = bytestring[n];
-      s = &var;
-      printf("This is the short int version: %p, and it has a size of %lu\n", s, sizeof(s));
-      int var2 = bytestring[n];
-      i = &var2;
-      printf("This is the int version: %p, and it has a size of %lu\n", i, sizeof(i));
-      long int var3 = bytestring[n];
-      l = &var3;
-      printf("This is the long int version: %p, and it has a size of %lu\n", l, sizeof(l));
+      printf("%02X, ", bytestring[n]);
    }
+   printf("\nShort int version: ");
+   unsigned short int *sb = ((unsigned short int*) (bytestring));
+   for (int n = 0; n < 4; n++)
+   {
+      printf("%04X, ", sb[n]);
+   }
+   printf("\nInt version: ");
+   unsigned int *ib = ((unsigned int*) (bytestring));
+   for (int n = 0; n < 2; n++)
+   {
+      printf("%08X, ", ib[n]);
+   }
+   printf("\nLong int version: ");
+   unsigned long int *lb = ((unsigned long int*) (bytestring));
+   printf("%lX", lb[0]);
+ 
+   
 }
