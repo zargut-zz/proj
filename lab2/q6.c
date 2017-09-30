8e9e7221a930e48671c7ffa41d0d2292be38557b
Rebecca Dong <rdong333@gmail.com>
2017-09-30 16:56:43 -0700

    Update q6.c

--- lab2/q6.c
+++ lab2/q6.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
 
 typedef struct _intfloat
-   {	
-      unsigned int sign;
-      int exponent;
-      int fraction;
-   } INTFLOAT, *INTFLOAT_PTR;
+{	
+   unsigned int sign;
+   int exponent;
+   int fraction;
+} INTFLOAT, *INTFLOAT_PTR;
+
+void extract_float(INTFLOAT_PTR x, float f)
+{
+   unsigned floatbits;
+   //float value;
+
+   if (f == 0)
+   {
+      x -> sign = 0;
+      x -> exponent = 0;
+      x -> fraction = 0;
+   }
+   else
+   {
+      floatbits = * (unsigned int *) &f;
+      //printf ("Floatbits: 0x%8X\n", floatbits);
+      //x -> sign = (floatbits & 0x80000000);
+      x->sign = (floatbits >> 31);
+      //printf ("Sign: 0x%08X\n", x->sign);
+      
+      x -> exponent = (floatbits >> 23)& 0x000000FF;
+      //printf ("exp1: 0x%08X\n", x->exponent);
+      x -> exponent -= 0x0000007F;
+
+      x -> fraction = (floatbits & 0x007FFFFF);      
+      
+      if (x->sign == 0x80000000)
+      {
+         x -> fraction = (~(x -> fraction)) + 1;
+         printf("NEGATIVE, ");
+      }
+      
+   }
+   printf ("Test case: 0x%8X\n", floatbits);
+   printf ("Float is: %f\n", f);
+   printf ("Fraction is: 0x%08X\n", x -> fraction);
+   printf ("Exponent is: %d\n", x -> exponent);
+   printf("\n"); 
+}
 
 float fsub(a,b)
 {
