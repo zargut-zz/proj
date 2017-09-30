d11ebe079b00eb7f87585b95cbed2ca23f6cde2c
Rebecca Dong <rdong333@gmail.com>
2017-09-30 16:53:36 -0700

    Started q6

---
+++ lab2/q6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct _intfloat
+   {	
+      unsigned int sign;
+      int exponent;
+      int fraction;
+   } INTFLOAT, *INTFLOAT_PTR;
+
+float fsub(a,b)
+{
+   INTFLOAT ax, bx;
+   INTFLOAT result;
+   int diffexp;
+   float retval;
+
+   
+}
+
+int main()
+{
+   return 1;
+}
