964442b82507e47d38fa00de135cdae874076fed
Rebecca Dong <rdong333@gmail.com>
2017-09-30 16:34:04 -0700

    Added q1 to main file

--- lab2/lab2_main.c
+++ lab2/lab2_main.c
@@ -12,6 +12,33 @@ typedef struct _intfloat{
    int fraction;
    }INTFLOAT, *INTFLOAT_PTR;
 
+unsigned int umultiply(unsigned int a, unsigned int b)
+{
+   unsigned int origA = a;
+   unsigned int origB = b;
+   
+   /*
+    * Shift right and add
+    */
+   
+   unsigned int c = 0;
+   while(a != 0)
+   {
+      if(a & 1)      // If the lowest order bit is set in A
+         c += b;
+      //OR        -> x <<= 1;
+                  // y >>= 1;  instead of what I have down below
+      a = a >> 1;
+      b = b + b;  // Alternatively, left-shift by 1 bit
+   }
+   
+   printf("%.2u * %.2u = %.2u\n", origA, origB, c);
+   printf("0x%02X * 0x%02X = 0x%02X\n", origA, origB, c);
+   
+   return c;
+   //printf("0x%02X + 0x%02X = 0x%02X \n", b1, b2, result);
+}
+
 void extract_float(INTFLOAT_PTR x, float f)
 {
    unsigned floatbits;
@@ -54,7 +81,34 @@ void extract_float(INTFLOAT_PTR x, float f)
 int main()
 {
    //Question 1
-  
+   unsigned int a = 0x0001;
+   unsigned int b = 0x0001;
+   umultiply(a, b);
+   
+   printf("\n");
+   
+   a = 0x0001;
+   b = 0xFFFF;
+   umultiply(a, b);
+   
+   printf("\n");
+   
+   a = 0x8000;
+   b = 0x0001;
+   umultiply(a, b);
+   
+   printf("\n");
+   
+   a = 0x4000;
+   b = 0x4000;
+   umultiply(a, b);
+   
+   printf("\n");
+   
+   a = 0x8000;
+   b = 0x8000;
+   umultiply(a, b);
+   
    //Question 2
    INTFLOAT INTFLOAT_PTR;
    printf ("2a. \n");
